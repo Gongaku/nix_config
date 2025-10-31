cb772410dfd146e38b184d0db6555dd9ce523cfe
Gongaku <132847661+Gongaku@users.noreply.github.com>
2025-10-31 11:27:23 -0600

    Updated system

--- modules/hosts/nixos/boot.nix
+++ modules/hosts/nixos/boot.nix
@@ -24,6 +24,5 @@
       kernelModules = [ "kvm-intel" ];
 		};
 		nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-		hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 	};
 }
--- modules/hosts/nixos/fileSystems.nix
+++ modules/hosts/nixos/fileSystems.nix
@@ -1,5 +1,7 @@
 {
-	flake.modules.nixos."hosts/nixos" = {
+	flake.modules.nixos."hosts/nixos" =
+	{ config, lib, ... }:
+	{
 		fileSystems."/" =
 			{ device = "/dev/disk/by-uuid/6361b35d-834c-4597-b258-92bd040aa9e8";
 				fsType = "ext4";
@@ -13,5 +15,7 @@
 
 		swapDevices =
 			[ { device = "/dev/disk/by-uuid/33e63045-03e5-4899-9461-85c12cb4a4d6"; } ];
+
+		hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 	};
 }
