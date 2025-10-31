d0c7cf43c2b0460c3c384d9da011f62f2da99c1e
Gongaku <132847661+Gongaku@users.noreply.github.com>
2025-10-30 22:39:20 -0600

    Updated system

--- modules/hosts/nixos/boot.nix
+++ modules/hosts/nixos/boot.nix
@@ -21,8 +21,8 @@
       ];
 
       kernelModules = [ "kvm-intel" ];
-			hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 		};
 		nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+		hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 	};
 }
