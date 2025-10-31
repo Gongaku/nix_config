4d614cf480f7771001970bd8d13ac199aaec6b71
Gongaku <132847661+Gongaku@users.noreply.github.com>
2025-10-30 22:45:10 -0600

    Updated system

---
+++ modules/flake-parts/nixpkgs.nix
@@ -0,0 +1,17 @@
+{
+	inputs,
+	withSystem,
+	...
+}:
+{
+	flake = {
+		overlays.default =
+			final: prev:
+			withSystem prev.stdenv.hostPlatform.system (
+				{ config, ... }:
+				{
+					local = config.packages;
+				}
+			);
+	};
+}
