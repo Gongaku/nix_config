a3a7f2ff664a11b3b5cefee724e23b357ae09171
Gongaku <132847661+Gongaku@users.noreply.github.com>
2025-10-31 15:21:13 -0600

    Added discord and librewolf configs

---
+++ modules/desktop/discord.nix
@@ -0,0 +1,55 @@
+{
+	flake.modules = {
+		homeManager.desktop = {
+			imports = [
+				inputs.nixcord.homeModules.nixcord
+			];
+
+			programs.nixcord = {
+				enable = true;
+				vesktop.enable = true;
+				config = {
+					autoUpdate = true;
+					autoUpdateNotification = true;
+					plugins = {
+						betterFolders.enable = true;
+						betterSettings.enable = true;
+						blurNSFW.enable = true;
+						callTimer.enable = true;
+						clearURLs.enable = true;
+						crashHandler.enable = true;
+						fakeNitro.enable = true;
+						favoriteGifSearch.enable = true;
+						fixSpotifyEmbeds.enable = true;
+						fixYoutubeEmbeds.enable = true;
+						friendInvites.enable = true;
+						gameActivityToggle.enable = true;
+						noF1.enable = true;
+						replaceGoogleSearch = {
+							enable = true;
+							customEngineName = "duckduckgo";
+							customEngineURL = "https://duckduckgo.com/?q=";
+						};
+						serverInfo.enable = true;
+						showMeYourName.enable = true;
+						spotifyControls.enable = true;
+						spotifyCrack.enable = true;
+						themeAttributes.enable = true;
+						translate.enable = true;
+						unlockedAvatarZoom.enable = true;
+						userMessagesPronouns.enable = true;
+						volumeBooster.enable = true;
+						webKeybinds.enable = true;
+						webScreenShareFixes.enable = true;
+						youtubeAdblock.enable = true;
+					};
+					themeLinks = [
+						"https://raw.githubusercontent.com/DiscordStyles/RadialStatus/deploy/RadialStatus.theme.css"
+						"https://raw.githubusercontent.com/maenDisease/BetterDiscordStuff/refs/heads/main/css/FloatingModal.css"
+						"https://raw.githubusercontent.com/refact0r/system24/refs/heads/main/theme/system24.theme.css"
+					];
+				};
+			};
+		};
+	};
+}
